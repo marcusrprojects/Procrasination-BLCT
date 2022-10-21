b74cc42dda98c5eb3f8df3467cb77dfb1772bd0a
mribeiro9 <mribeiro@bowdoin.edu>
2022-10-21 15:07:06 -0400

    Initial Commit

---
+++ Procrasination-BLCT/Procrasination_BLCTApp.swift
@@ -0,0 +1,17 @@
+//
+//  Procrasination_BLCTApp.swift
+//  Procrasination-BLCT
+//
+//  Created by Marcus Ribeiro on 10/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Procrasination_BLCTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
